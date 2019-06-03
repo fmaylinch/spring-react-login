30ad1ddd40701eb2a8b1e8abcfdbfa61944a51a2
fmaylinch <ferranmaylinch@gmail.com>
2019-06-03 10:51:37 +0200

    Create simple Main React component

---
+++ src/webapp/src/components/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Main extends React.Component {
+
+    render() {
+
+        return (
+            <div>
+                <h1>Notes App</h1>
+                <h2>Notes:</h2>
+                <ul>
+                    <li>One note</li>
+                    <li>Another note</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Main;
